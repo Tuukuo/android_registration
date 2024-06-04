708f641ed7a28e481a280fca428f512acae8fca3
Tuukuo <tuukuodorcas@gmail.com>
2024-06-04 22:31:41 +0300

    first commit

--- app/src/main/java/com/akirachix/mentorconnect/loginPage.kt
+++ app/src/main/java/com/akirachix/mentorconnect/loginPage.kt
@@ -1,22 +1,22 @@
 package com.akirachix.mentorconnect
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class loginPage : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val tvLogin=findViewById<TextView>(R.id.tvLogin)
-        tvLogin.setOnClickListener{
-            val intent=Intent(this,login::class.java)
+        setContentView(R.layout.activity_login_page)
+         val tvSignIn = findViewById<TextView>(R.id.tvSignIn)
+        tvSignIn.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-        }
     }
+}
